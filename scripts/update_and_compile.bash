f30787b36dbd4248c9b83fa255412c8e00a0ef90
Tomek Winiarski <tomrobotics@gmail.com>
2015-11-26 14:30:07 -0800

    fix

--- scripts/update_and_compile.bash
+++ scripts/update_and_compile.bash
@@ -1,20 +1,12 @@
 #!/bin/bash
+# Skrypt powinien być wołan z katalogu robot/src/sarkofag_robot/scripts
 
+
+source /opt/ros/indigo/setup.bash
 export LANG=en_US.UTF-8
-export LANGUAGE=en
+export LANG=en
 
-
-if [ ! -d $1 ]; then
-  mkdir $1
-fi
-
-cd $1
-wstool init
-
-wget https://raw.githubusercontent.com/RCPRG-ros-pkg/RCPRG_rosinstall/master/sarkofag.rosinstall -O /tmp/sarkofag.rosinstall
-
-
-wstool merge /tmp/sarkofag.rosinstall
+wstool merge /tmp/sark.rosinstall
 wstool update
 cd underlay_isolated
 catkin_make_isolated --install -DENABLE_CORBA=ON -DCORBA_IMPLEMENTATION=OMNIORB -DCMAKE_BUILD_TYPE=RelWithDebInfo
