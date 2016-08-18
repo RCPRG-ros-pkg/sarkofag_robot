6e1f182376cd2e56bb0d6f127347ee6ce41f7fc6
megierek <wegierek.maciej@gmail.com>
2016-08-18 18:07:44 +0200

    instalacja

--- scripts/sarkofag_setup.sh
+++ scripts/sarkofag_setup.sh
@@ -3,15 +3,17 @@
 export LANG=en_US.UTF-8
 export LANGUAGE=en
 
-wget https://raw.githubusercontent.com/RCPRG-ros-pkg/sarkofag_robot/master/scripts/sarkofag.rosinstall -O /tmp/sark.rosinstall
-wget https://raw.githubusercontent.com/RCPRG-ros-pkg/sarkofag_robot/master/scripts/update_and_compile.bash -O /tmp/update_and_compile.bash
-
-if [ ! -d $1 ]; then
-  mkdir $1
-fi
-
-cd $1
-wstool init
-
-bash /tmp/update_and_compile.bash
-touch $1/robot/src/sarkofag_robot/scripts/hardware
+read -r -p "Are you now in a directory where you want to store sarkofag repositories? [y/N] " response
+case $response in
+    [yY][eE][sS]|[yY]) 
+        wget https://raw.githubusercontent.com/RCPRG-ros-pkg/sarkofag_robot/master/scripts/sarkofag.rosinstall -O /tmp/sark.rosinstall
+	#wget https://raw.githubusercontent.com/RCPRG-ros-pkg/sarkofag_robot/master/scripts/update_and_compile.bash -O /tmp/update_and_compile.bash
+	wget https://github.com/mwegiere/sarkofag_robot/blob/master/scripts/update_and_compile.bash -O /tmp/update_and_compile.bash
+	wstool init
+	bash /tmp/update_and_compile.bash
+	touch robot/src/sarkofag_robot/scripts/hardware
+        ;;
+    *)
+        echo "Run this script in a directory where you want to store sarkofag repositories"
+        ;;
+esac
